2e200fba6ef0460bd88e92bb7bf4bc5e18393086
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-06-18 23:48:53 -0400

    added different colors for each command

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@
 #include <vector>
 #include <thread>
 
+const char* COLORS[] = {
+    "\033[32m", // Green
+    "\033[36m", // Cyan
+    "\033[33m", // Yellow
+    "\033[35m", // Magenta
+    "\033[34m", // Blue
+    "\033[91m", // Bright Red
+    "\033[92m", // Bright Green
+    "\033[93m", // Bright Yellow
+    "\033[94m", // Bright Blue
+    "\033[95m", // Bright Magenta
+};
+
 /// Executes a shell command with stdout and stderr merged into one pipe.
 ///
 /// This function redirects both stdout and stderr to the provided file descriptor
@@ -29,11 +42,12 @@ void exec_command(const char* cmd, int write_fd) {
 ///
 /// @param cmd The command name to display in the output prefix.
 /// @param read_fd File descriptor to read from.
-void handle_output(const char* cmd, int read_fd) {
+/// @param color ANSI color code string to use for prefix.
+void handle_output(const char* cmd, int read_fd, const char* color) {
     FILE* stream = fdopen(read_fd, "r");
     char buffer[4096];
     while (fgets(buffer, sizeof(buffer), stream)) {
-        std::cout << "\033[32m[" << cmd << "]\033[0m " << buffer;
+        std::cout << color << "[" << cmd << "]\033[0m " << buffer;
     }
     fclose(stream);
 }
@@ -44,7 +58,7 @@ void handle_output(const char* cmd, int read_fd) {
 /// through a pipe. Output is prefixed with the command name and printed in green.
 ///
 /// @param cmd The shell command to execute.
-void run_command(const char* cmd) {
+void run_command(const char* cmd, const char* color) {
     int pipefd[2];
     pipe(pipefd);
 
@@ -56,7 +70,7 @@ void run_command(const char* cmd) {
     } else {
         // In parent
         close(pipefd[1]);
-        handle_output(cmd, pipefd[0]);
+        handle_output(cmd, pipefd[0], color);
         int status;
         waitpid(child, &status, 0);
     }
@@ -67,9 +81,13 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: " << argv[0] << " <command1> <command2> ..." << std::endl;
         return 1;
     }
+
+    const int color_count = sizeof(COLORS) / sizeof(COLORS[0]);
     std::vector<std::thread> threads;
+
     for (int i = 1; i < argc; i++) {
-        threads.emplace_back(run_command, argv[i]);
+        const char* color = COLORS[(i - 1) % color_count];
+        threads.emplace_back(run_command, argv[i], color);
     }
     for (auto& thread : threads) {
         thread.join();
